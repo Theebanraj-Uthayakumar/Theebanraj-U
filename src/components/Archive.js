6288bb061e8ae317d5e22ddef5c414871394d13b
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-11-20 17:30:01 +0530

    first commit

---
+++ src/components/Archive.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Archive() {
+  return <div>Thi is Archive page</div>;
+}
+
+export default Archive;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <span>This is home page</span>
+      <br />
+      <a href="/Archive">Archive</a>
+    </div>
+  );
+}
+
+export default Home;
